362fe0538832a75b2e64a2cf844a8d5571c930c1
Alejandro Diaz <alejandro6uuu6@gmail.com>
2023-11-18 13:59:13 -0500

    imageview account con posibilidad para subir fotos de galeria y tomar fotos de la camara

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
-
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/idat/foodie_app/NavbarFragment/AccountFragment.kt
+++ app/src/main/java/com/idat/foodie_app/NavbarFragment/AccountFragment.kt
@@ -5,7 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import android.content.Intent
+import android.content.DialogInterface
+import android.app.Activity.RESULT_OK
+import android.provider.MediaStore
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.idat.foodie_app.R
+import android.app.Activity
+import android.graphics.Bitmap
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,14 +42,80 @@ class AccountFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
+    private val PICK_IMAGE_REQUEST = 1
+    private val TAKE_PHOTO_REQUEST = 2
+    private lateinit var floatingActionButton: FloatingActionButton
+    private lateinit var imageView2: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        val view = inflater.inflate(R.layout.fragment_account, container, false)
+
+        floatingActionButton = view.findViewById(R.id.floatingActionButton)
+        imageView2 = view.findViewById(R.id.imageView2)
+
+        floatingActionButton.setOnClickListener {
+            showImageSourceDialog()
+        }
+
+        return view
     }
+
+    private fun showImageSourceDialog() {
+        val options = arrayOf("Cámara", "Galería")
+
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Seleccionar Fuente de Imagen")
+            .setItems(options) { dialog, which ->
+                when (which) {
+                    0 -> takePhoto()
+                    1 -> pickImageFromGallery()
+                }
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    private fun takePhoto() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivityForResult(takePictureIntent, TAKE_PHOTO_REQUEST)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        imageView2 = view?.findViewById(R.id.imageView2)!!
+
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                PICK_IMAGE_REQUEST -> {
+                    // Resultado de la galería
+                    if (data != null && data.data != null) {
+                        val imageUri = data.data
+                        imageView2.setImageURI(imageUri)
+                    }
+                }
+                TAKE_PHOTO_REQUEST -> {
+                    // Resultado de la cámara
+                    val imageBitmap = data?.extras?.get("data") as Bitmap?
+                    if (imageBitmap != null) {
+                        imageView2.setImageBitmap(imageBitmap)
+                    }
+                }
+            }
+        }
+    }
+
+
 
     companion object {
         /**
